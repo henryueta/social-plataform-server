f1cd8ca62c943c7ee62e64ac77ea149b1067c8d9
henryueta <henrykenjiueta@gmail.com>
2025-08-19 17:08:35 -0300

    databaseKeysEnv

--- api/config/database.js
+++ api/config/database.js
@@ -1,8 +1,8 @@
 const {createClient} = require('@supabase/supabase-js')
+const url = process.env.DATABASE_SERVICE_URL;
+const role = process.env.DATABASE_SERVICE_ROLE;
 
-const supabase = createClient(
-    "https://dpjzdzhdhqgackpxojdi.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwanpkemhkaHFnYWNrcHhvamRpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTQ4NDczOCwiZXhwIjoyMDY3MDYwNzM4fQ.eAN0skXy5phQyvD4RO3HFaUfXP1-D0So61qVck8jSU0")
+const supabase = createClient(url, role)
 
 
 module.exports =  {
--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require("cors")
 const {user_router}  = require("./routes/user-route.js")
